33c592eef838c3253ed6ffc3cfee37c0f7804634
Apurv Gupta <apurv3351@gmail.com>
2020-10-26 01:39:23 +0530

    added forgot password

--- client/src/components/NavBar/SignIn.js
+++ client/src/components/NavBar/SignIn.js
@@ -18,19 +18,28 @@ const styles = () => ({
 	},
 	start: {
 		flexDirection: "column",
-		marginTop: "-30%",
 	},
 	login: {
 		display: "flex",
 		top: "30%",
 		flexDirection: "column",
-		padding: "0% 5%",
+		padding: "2% 5%",
 		textAlign: "center",
 		position: "absolute",
 		justifyContent: "center",
 	},
 	signIn: {
-		marginBottom: "5%",
+		marginBottom: "3%",
+	},
+	forgotPassword: {
+		padding: "5% 5%",
+		position: "absolute",
+		marginTop: "30%",
+		justifyContent: "center",
+		textAlign: "center",
+	},
+	forgot: {
+		margin: "2%",
 	},
 	textField: {
 		marginTop: "2%",
@@ -41,7 +50,9 @@ const styles = () => ({
 		color: "red",
 		marginBottom: "2%",
 	},
-	bottom: {},
+	button: {
+		marginBottom: "5%",
+	},
 });
 
 function SignIn(props) {
@@ -51,7 +62,7 @@ function SignIn(props) {
 	const [password, setPassword] = useState("");
 	const [open, setOpen] = useState(false);
 	const [errors, setErrors] = useState({});
-	const [user, setUser] = useState("");
+	const [forgot, setForgot] = useState(false);
 	// const [details, setDetails] = useState("");
 
 	// function handleChangeEmail(event) {
@@ -104,6 +115,17 @@ function SignIn(props) {
 				});
 		}
 	};
+
+	const handleForgotPassword = () => {
+		if (
+			!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+				email
+			)
+		) {
+			return setErrors({ email: "Invalid Email!" });
+		}
+	};
+
 	function handleClickOpen() {
 		setOpen(true);
 	}
@@ -133,113 +155,127 @@ function SignIn(props) {
 				classes={{
 					paper: classes.paper,
 				}}>
-				app
 				<img
 					alt="loginBg"
 					src="https://res.cloudinary.com/rweb1/image/upload/v1600243283/Assets/images/loginBg_olbayb.svg"
 					style={{ visibility: "initial", width: "580px" }}
 				/>
-				{/* {
-					user ?
-					user==="student" && (
-						<div>
-
-						</div>
-					) :
-						<div>
-							
-						</div>
-				} */}
-				{!user && (
-					<div className={classes.start}>
+				{!forgot ? (
+					<div className={classes.login}>
+						<Typography variant="h4" color="primary" className={classes.signIn}>
+							Student Log in
+						</Typography>
+						<Typography variant="inherit" color="primary" className={classes.signIn}>
+							(Only enrolled students can login, kindly contact the administrator)
+						</Typography>
+						{/* <form noValidate> */}
+						<TextField
+							id="email"
+							name="email"
+							type="email"
+							label="Email"
+							variant="outlined"
+							color="secondary"
+							// helperText={!email} //error was defined in the backend
+							// error={errors.email ? true : false}
+							className={classes.textField}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							autoComplete="off"
+							fullWidth
+						/>
+						<TextField
+							id="password"
+							name="password"
+							type="password"
+							label="Password"
+							variant="outlined"
+							color="secondary"
+							helperText={errors.password} //error was defined in the backend
+							error={errors.password ? true : false}
+							className={classes.textField}
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							fullWidth
+						/>
+						{errors.email && (
+							<Typography variant="body2" className={classes.customError}>
+								{errors.email}
+							</Typography>
+						)}
+						{errors.password && (
+							<Typography variant="body2" className={classes.customError}>
+								{errors.password}
+							</Typography>
+						)}
+						{errors.err && (
+							<Typography variant="body2" className={classes.customError}>
+								{errors.err}
+							</Typography>
+						)}
 						<Button
 							type="submit"
 							variant="contained"
 							color="secondary"
 							// className={classes.button}
-							// size="large"
-							onClick={() => setUser("student")}>
-							Student
+							size="large"
+							fullWidth
+							onClick={() => handleSubmit()}>
+							Login
 						</Button>
+						<br />
+						<br />
+						<Typography variant="inherit" color="primary" className={classes.forgot}>
+							Forgot{" "}
+							<span style={{ color: "#0F7DC2", cursor: "pointer" }} onClick={() => setForgot(true)}>
+								Password?
+							</span>
+						</Typography>
+					</div>
+				) : (
+					<div className={classes.forgotPassword}>
+						<Typography variant="h4" color="primary" className={classes.signIn}>
+							Forgot Password?
+						</Typography>
+						<TextField
+							id="email"
+							name="email"
+							type="email"
+							label="Email"
+							variant="outlined"
+							color="secondary"
+							// helperText={!email} //error was defined in the backend
+							// error={errors.email ? true : false}
+							className={classes.textField}
+							style={{ marginTop: "10%" }}
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							autoComplete="off"
+							fullWidth
+						/>
+						{errors.email && (
+							<Typography variant="body2" className={classes.customError}>
+								{errors.email}
+							</Typography>
+						)}
 						<Button
 							type="submit"
 							variant="contained"
 							color="secondary"
-							// className={classes.button}
-							// size="large"
-							onClick={() => setUser("admin")}>
-							Admin
+							className={classes.button}
+							size="large"
+							fullWidth
+							onClick={() => handleForgotPassword()}>
+							Forgot Password
 						</Button>
+						<Typography variant="inherit" color="primary">
+							Click here to go{" "}
+							<span style={{ color: "#0F7DC2", cursor: "pointer" }} onClick={() => setForgot(false)}>
+								back.
+							</span>
+						</Typography>
 					</div>
 				)}
-				{/* <div className={classes.login}>
-					<Typography variant="h4" color="primary" className={classes.signIn}>
-						Student Log in
-					</Typography>
-					<Typography variant="inherit" color="primary" className={classes.signIn}>
-						(Only enrolled students can login, kindly contact the administrator)
-					</Typography> */}
-				{/* <form noValidate> */}
-				{/* <TextField
-						id="email"
-						name="email"
-						type="email"
-						label="Email"
-						variant="outlined"
-						color="secondary"
-						// helperText={!email} //error was defined in the backend
-						// error={errors.email ? true : false}
-						className={classes.textField}
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						autoComplete="off"
-						fullWidth
-					/> */}
-				{/* <TextField
-						id="password"
-						name="password"
-						type="password"
-						label="Password"
-						variant="outlined"
-						color="secondary"
-						helperText={errors.password} //error was defined in the backend
-						error={errors.password ? true : false}
-						className={classes.textField}
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						fullWidth
-					/> */}
-				{/* {errors.email && (
-						<Typography variant="body2" className={classes.customError}>
-							{errors.email}
-						</Typography>
-					)}
-					{errors.password && (
-						<Typography variant="body2" className={classes.customError}>
-							{errors.password}
-						</Typography>
-					)}
-					{errors.err && (
-						<Typography variant="body2" className={classes.customError}>
-							{errors.err}
-						</Typography>
-					)}
-					<Button
-						type="submit"
-						variant="contained"
-						color="secondary"
-						className={classes.button}
-						size="large"
-						fullWidth
-						onClick={() => handleSubmit()}>
-						Login
-					</Button>
-					<br />
-					<br />
-					<Typography variant="inherit" color="primary" className={classes.forgot}>
-						Forgot <span style={{ color: "#0F7DC2" }}>Password?</span>
-					</Typography>
-				</div> */}
 			</Dialog>
 		</div>
 	);
